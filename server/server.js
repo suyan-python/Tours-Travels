7cb9965f97666b33657f713fab260dcebd3cea2e
Suyan Man Amatya <srkamatya007@gmail.com>
2025-07-19 18:03:43 +0545

    new back

--- server/server.js
+++ server/server.js
@@ -11,46 +11,37 @@ import roomRouter from "./routes/roomRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
-const startServer = async () => {
-  try {
-    await connectDB(); // Wait for MongoDB connection before starting
-    connectCloudinary(); // Initialize Cloudinary
+connectDB(); // Wait for MongoDB connection before starting
+connectCloudinary(); // Initialize Cloudinary
 
-    const app = express();
+const app = express();
 
-    app.use(cors());
+app.use(cors());
 
-    //  API to listen to Stripe Webhooks
-    app.post(
-      "/api/stripe",
-      express.raw({ type: "application/json" }),
-      stripeWebhooks
-    );
+//  API to listen to Stripe Webhooks
+// app.post(
+//   "/api/stripe",
+//   express.raw({ type: "application/json" }),
+//   stripeWebhooks
+// );
 
-    app.use(express.json());
-    app.use(clerkMiddleware());
+app.use(express.json());
+app.use(clerkMiddleware());
 
-    // Clerk webhook route
-    app.use("/api/clerk", clerkWebhooks);
+// Clerk webhook route
+app.use("/api/clerk", clerkWebhooks);
 
-    // API routes
-    app.use("/api/user", userRouter);
-    app.use("/api/hotels", hotelRouter);
-    app.use("/api/rooms", roomRouter);
-    app.use("/api/bookings", bookingRouter);
+// API routes
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
 
-    app.get("/", (req, res) => {
-      res.send("API is working");
-    });
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to DB:", error);
-    process.exit(1); // Exit process on DB failure
-  }
-};
-
-startServer();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
