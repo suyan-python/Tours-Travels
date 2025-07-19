a2a8f635d07e06a63ed232264198c1e2ac15fe4a
Suyan Man Amatya <srkamatya007@gmail.com>
2025-07-19 17:55:16 +0545

    newnew

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -5,7 +5,7 @@ import { exclusiveOffers } from "../assets/assets";
 
 const ExclusiveOffers = () => {
   return (
-    <div className="flex flex-col items-center px-6 md: px-16 lg:px-24 xl:px-32 pt-20 pb-30">
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30">
       <div className="flex flex-col md:flex-row items-center justify-between w-full">
         <Title
           align="left"
@@ -27,7 +27,7 @@ const ExclusiveOffers = () => {
         {exclusiveOffers.map((item) => (
           <div
             key={item._id}
-            className="group relative flex flex-col items-start justify-between gap-1 pt-12 md: pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center"
+            className="group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center"
             style={{ backgroundImage: ` url(${item.image})` }}
           >
             <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full">
