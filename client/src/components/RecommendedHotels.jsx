5c37713c846fafe71037e8757903dd87431a58be
Suyan Man Amatya <srkamatya007@gmail.com>
2025-07-22 20:31:20 +0545

    last push

--- client/src/components/RecommendedHotels.jsx
+++ client/src/components/RecommendedHotels.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 
 import HotelCard from "./HotelCard";
 import Title from "./Title";
-import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
 import { useState } from "react";
 
